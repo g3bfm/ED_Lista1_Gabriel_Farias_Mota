3b70ab516f2cc91a09ce90b3d9c6fc8a4f2bf0bd
Gabriel Mota <gabriel.mota70@outlook.com>
2026-03-13 18:58:03 -0400

    Exercício1.c

--- Lista1/Exercício1.c
+++ Lista1/Exercício1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main (){
+	
+	int l[6]={1, 0, 5, -2, -5, 7}, soma, i;
+	
+	soma = l[0] + l[1] + l[5];
+	printf("Soma: %d\n", soma);
+	
+	l[4] = 100;
+	
+	for(i = 0; i < 6; i++){
+		printf("%d\n", l[i]);
+	}
+	
+	return 0;
+	
+}
